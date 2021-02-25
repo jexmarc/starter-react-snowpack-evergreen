063db822235e69e255481156207294fc51f1f07b
Marc Gallagher <marc@ammoseek.com>
2021-02-24 21:40:44 -0500

    initial commit

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,11 +1,10 @@
 /** @type {import("snowpack").SnowpackUserConfig } */
 module.exports = {
   mount: {
-    /* ... */
+    public: '/',
+    src: '/dist',
   },
-  plugins: [
-    /* ... */
-  ],
+  plugins: [['@snowpack/plugin-react-refresh']],
   routes: [
     /* Enable an SPA Fallback in development: */
     // {"match": "routes", "src": ".*", "dest": "/index.html"},
@@ -23,4 +22,4 @@ module.exports = {
   buildOptions: {
     /* ... */
   },
-};
+}
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Pane, Heading } from 'evergreen-ui'
+
+const App = () => {
+  return (
+    <Pane>
+      <Heading>Main Page</Heading>
+    </Pane>
+  )
+}
+
+export default App
